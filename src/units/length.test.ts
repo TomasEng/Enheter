780f3e3a27ff06e01502d26ced91f191b4b81832
Tomas <tomas.engebretsen@gmail.com>
2023-04-12 22:06:19 +0200

    Add some length units

--- src/units/length.test.ts
+++ src/units/length.test.ts
@@ -5,4 +5,25 @@ describe('length', () => {
     '%s has correct dimension',
     (unit) => expect(lengthUnits[unit].dimension).toEqual({length: 1})
   );
+
+  test('Units have correct relation to the metre', () => {
+    expect(lengthUnits.angstrom.toBase(1)).toBeCloseTo(1e-10);
+    expect(lengthUnits.astronomicalUnit.toBase(1)).toBeCloseTo(149597870700);
+    expect(lengthUnits.chain.toBase(1)).toBeCloseTo(20.1168);
+    expect(lengthUnits.fathom.toBase(1)).toBeCloseTo(1.8288);
+    expect(lengthUnits.fermi.toBase(1)).toBeCloseTo(1e-15);
+    expect(lengthUnits.foot.toBase(1)).toBeCloseTo(0.3048);
+    expect(lengthUnits.furlong.toBase(1)).toBeCloseTo(201.168);
+    expect(lengthUnits.inch.toBase(1)).toBeCloseTo(0.0254);
+    expect(lengthUnits.lightYear.toBase(1)).toBeCloseTo(9460730472580800);
+    expect(lengthUnits.link.toBase(1)).toBeCloseTo(0.201168);
+    expect(lengthUnits.metre.toBase(1)).toBeCloseTo(1);
+    expect(lengthUnits.micron.toBase(1)).toBeCloseTo(1e-6);
+    expect(lengthUnits.nauticalMile.toBase(1)).toBeCloseTo(1852);
+    expect(lengthUnits.parsec.toBase(1)).toBeCloseTo(30856775814913600);
+    expect(lengthUnits.rod.toBase(1)).toBeCloseTo(5.0292);
+    expect(lengthUnits.scandinavianMile.toBase(1)).toBeCloseTo(10000);
+    expect(lengthUnits.statuteMile.toBase(1)).toBeCloseTo(1609.344);
+    expect(lengthUnits.yard.toBase(1)).toBeCloseTo(0.9144);
+  });
 });
--- src/units/length.ts
+++ src/units/length.ts
@@ -1,21 +1,35 @@
 import {UnitList} from '../UnitList';
 import {metre} from './basicUnits';
 
+const inch = metre.withFactor(0.0254, 'in');
+const foot = inch.withFactor(12, 'ft');
+const yard = foot.withFactor(3, 'yd');
+const fathom = yard.withFactor(2, 'fathom');
+const chain = fathom.withFactor(11, 'chain');
+const rod = chain.withFactor(1 / 4, 'rod');
+const link = chain.withFactor(1 / 100, 'link');
+const furlong = chain.withFactor(10, 'furlong');
+const statuteMile = furlong.withFactor(8, 'mi');
+
 export const lengthUnits: UnitList = {
   angstrom: metre.withFactor(1e-10, 'Å'),
   astronomicalUnit: metre.withFactor(149597870700, 'AU'),
-  fathom: metre.withFactor(1.8288, 'fathom'),
-  foot: metre.withFactor(0.3048, 'ft'),
-  furlong: metre.withFactor(201.168, 'furlong'),
-  inch: metre.withFactor(0.0254, 'in'),
+  chain,
+  fathom,
+  fermi: metre.withFactor(1e-15, 'fm'),
+  foot,
+  furlong,
+  inch,
   lightYear: metre.withFactor(9460730472580800, 'ly'),
+  link,
   metre,
   micron: metre.withFactor(1e-6, 'μm'),
   nauticalMile: metre.withFactor(1852, 'NM'),
   parsec: metre.withFactor(30856775814913600, 'pc'),
+  rod,
   scandinavianMile: metre.withFactor(10000, 'mil'),
-  statuteMile: metre.withFactor(1609.344, 'mi'),
-  yard: metre.withFactor(0.9144, 'yd'),
+  statuteMile,
+  yard,
 };
 
 export type LengthUnit = keyof typeof lengthUnits;
