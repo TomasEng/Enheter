cb86554c013935f1c2587198b0ad6e83354916d5
Tomas <tomas.engebretsen@gmail.com>
2023-04-28 20:41:49 +0200

    Adjust exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {Dimension} from './Dimension';
 export {Unit} from './Unit';
 export {Measure} from './Measure';
+export {BijectiveOperationChain, BijectiveOperationWithParam} from './BijectiveOperation'
 export * from './units';
