3af2651f8ce23aedb51ee9b9baeea7ad67797472
Tomas <tomas.engebretsen@gmail.com>
2023-05-13 16:18:05 +0200

    Export prefix and findDimensionName

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ export {Measure} from './Measure';
 export {BijectiveOperationChain, BijectiveOperationWithParam} from './BijectiveOperation';
 export type {DimensionName} from './types/DimensionName';
 export type {UnitName} from './types/UnitName';
+export type {Prefix} from './Prefix';
+export {findDimensionName} from './utils/findDimensionName';
 export * from './units';
