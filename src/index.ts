f4db6a64c6a972c868961f4201b8f61a948a8670
Tomas <tomas.engebretsen@gmail.com>
2023-05-13 16:26:13 +0200

    Export findUnitsByDimension

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ export type {DimensionName} from './types/DimensionName';
 export type {UnitName} from './types/UnitName';
 export type {Prefix} from './Prefix';
 export {findDimensionName} from './utils/findDimensionName';
+export {findUnitsByDimension} from './utils/findUnitsByDimension';
 export * from './units';
