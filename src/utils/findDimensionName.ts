cae83269bded2b1664f2744ed3598c636b9921f9
Tomas <tomas.engebretsen@gmail.com>
2023-05-27 13:51:13 +0200

    Set return type of findDimensionName

--- src/utils/findDimensionName.ts
+++ src/utils/findDimensionName.ts
@@ -1,7 +1,8 @@
 import {Dimension, dimensionsEqual} from '../Dimension';
 import {getAllUnitLists} from './getAllUnitLists';
+import {DimensionName} from '../types/DimensionName';
 
-export const findDimensionName = (dimension: Dimension): string | undefined =>
+export const findDimensionName = (dimension: Dimension): DimensionName | undefined =>
   getAllUnitLists()
     .find(unitList => dimensionsEqual(unitList.dimension, dimension))
-    ?.dimensionName;
+    ?.dimensionName as DimensionName;
